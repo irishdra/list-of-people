f5c513dd4a5e6775c14b3a84697b99b315ee3bbc
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2022-10-14 20:20:33 +0300

    Add redux & change table

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,88 +1,60 @@
 import React from 'react';
-import {DataGrid} from '@mui/x-data-grid';
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+} from '@mui/material';
 
-const rows = [
-  {
-    id: 1,
-    firstName: 'Frozen',
-    lastName: 'Yogurt',
-    phone: '095',
-    gender: true,
-    age: 100,
-  },
-  {
-    id: 2,
-    firstName: 'Ice cream',
-    lastName: 'Sandwich',
-    phone: '096',
-    gender: false,
-    age: 7,
-  },
-  {
-    id: 3,
-    firstName: 'Ice cream',
-    lastName: 'Sandwich',
-    phone: '096',
-    gender: false,
-    age: 7,
-  },
-  {
-    id: 4,
-    firstName: 'Ice cream',
-    lastName: 'Sandwich',
-    phone: '096',
-    gender: false,
-    age: 7,
-  },
-  {
-    id: 5,
-    firstName: 'Ice cream',
-    lastName: 'Sandwich',
-    phone: '096',
-    gender: false,
-    age: 7,
-  },
-  {
-    id: 6,
-    firstName: 'Ice cream',
-    lastName: 'Sandwich',
-    phone: '096',
-    gender: false,
-    age: 7,
-  },
-  {
-    id: 7,
-    firstName: 'Ice cream',
-    lastName: 'Sandwich',
-    phone: '096',
-    gender: false,
-    age: 7,
-  },
-];
-
-const columns = [
-  {field: 'id', headerName: 'ID', width: 70},
-  {field: 'firstName', headerName: 'First Name', width: 130},
-  {field: 'lastName', headerName: 'Last Name', width: 130},
-  {field: 'phone', headerName: 'Phone', width: 130},
-  {field: 'gender', headerName: 'Gender', width: 130},
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-];
-
-export const List = () => {
+export const List = ({data, onDelete}) => {
   return (
-    <div style={{height: 280, width: '85%', margin: '0 auto'}}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-      />
-    </div>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">First Name</TableCell>
+            <TableCell align="center">Last Name</TableCell>
+            <TableCell align="center">Phone</TableCell>
+            <TableCell align="center">Gender</TableCell>
+            <TableCell
+              align="center"
+            >
+              Age
+            </TableCell>
+            <TableCell align="center">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{'&:last-child td, &:last-child th': {border: 0}}}
+            >
+              <TableCell align="center">
+                {row.firstName}
+              </TableCell>
+              <TableCell align="center">{row.lastName}</TableCell>
+              <TableCell align="center">{row.phone}</TableCell>
+              <TableCell align="center">
+                {row.gender ? 'Male': 'Female'}
+              </TableCell>
+              <TableCell align="center">{row.age}</TableCell>
+              <TableCell align="center">
+                <Button
+                  onClick={() => onDelete(row.id)}
+                  variant="contained"
+                >
+                  Delete
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import {Container} from '@mui/material';
+import {v4 as uuidv4} from 'uuid';
+
+import {addRecord, deleteRecord} from '../store/actions';
 
 import {Form} from '../components/form/Form';
 import {List} from '../components/list/List';
 
 const Home = () => {
-  const addNewRecord = (data) => {
-    console.log('onSubmit', data);
+  const dispatch = useDispatch();
+  const records = useSelector((state) => state.records);
+
+  const onAddNewRecord = (data) => {
+    const newId = uuidv4();
+    dispatch(addRecord(data, newId));
+  };
+
+  const onDeleteRecord = (recordId) => {
+    dispatch(deleteRecord(recordId));
   };
 
   return (
     <Container>
-      <Form onSubmit={addNewRecord} />
-      <List />
+      <Form onSubmit={onAddNewRecord} />
+      <List data={records} onDelete={onDeleteRecord} />
     </Container>
   );
 };
